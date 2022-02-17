597789572e7ec3bf84e83a2e8be6c7455b265f6a
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-02-17 09:41:46 -0300

    ajustes req 5

--- src/components/Jogo.jsx
+++ src/components/Jogo.jsx
@@ -1,52 +1,80 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchPerguntas } from '../redux/actions';
-import Header from './Header';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchPerguntas } from "../redux/actions";
+import Header from "./Header";
 
 class Jogo extends Component {
-  componentDidMount() {
-    this.buscarPerguntas();
+  constructor() {
+    super();
+    this.state = {
+      respostas: [],
+      perguntas: [],
+    }
+    this.arrayRespostas = this.arrayRespostas.bind(this);
   }
 
-  buscarPerguntas() {
+  componentDidMount() {
+    console.log('Mount');
+    this.buscarPerguntas();
+    this.arrayRespostas(0);
+  }
+
+  // componentDidUpdate() {
+  //   console.log('Update');
+  //   this.arrayRespostas(0);
+  // }
+
+
+  async buscarPerguntas() {
     const { token, getPerguntas } = this.props;
-    console.log('didMount');
-    localStorage.setItem('token', JSON.stringify(token));
-    getPerguntas(token);
+    localStorage.setItem("token", JSON.stringify(token));
+    await getPerguntas(token);
+  }
+
+  arrayRespostas(index) {
+    const { perguntas } = this.state;
+    // const accPergunts = [];
+    // const res = [0, 1, 2, 3];
+    // let list = [0, 1, 2, 3];
+    // accPergunts.push({correct_answer: perguntas[index].correct_answer });
+    // perguntas[index].incorrect_answers.forEach(incorrect => {
+    //   accPergunts.push({ incorrect_answers: incorrect });
+    // });
+   
+    // list = list.sort(() => Math.random() - 0.5);
+    // accPergunts.forEach((element, i) => {
+    //   res.splice([list[i]], 1, element);
+    // });
+    // this.setState({ respostas: res });
+    // console.log(res);
   }
 
   render() {
     const { perguntas } = this.props;
+    if (!perguntas) return <h1>Loading...</h1>;
+    console.log();
     return (
       <section>
         <Header />
-        {perguntas === undefined ? (
-          'Loading'
-        ) : (
-          <>
-            {perguntas.map((pergunta, i) => (
-              <section key={ i }>
-                <h3 data-testid="question-category">{pergunta.category}</h3>
-                <p data-testid="question-text">{pergunta.question}</p>
-                <ul data-testid="answer-options">
-                  <button type="button" data-testid="correct-answer">
-                    {pergunta.correct_answer}
-                  </button>
-                  {pergunta.incorrect_answers.map((incorreta, index) => (
-                    <button
-                      type="button"
-                      key={ index }
-                      data-testid={ `wrong-answer-${index}` }
-                    >
-                      {incorreta}
-                    </button>
-                  ))}
-                </ul>
-              </section>
+        <section>
+          <h3 data-testid="question-category">{perguntas[0].category}</h3>
+          <p data-testid="question-text">{perguntas[0].question}</p>
+          <ul data-testid="answer-options">
+            <button type="button" data-testid="correct-answer">
+              {perguntas[0].correct_answer}
+            </button>
+            {perguntas[0].incorrect_answers.map((incorreta, index) => (
+              <button
+                type="button"
+                key={index}
+                data-testid={`wrong-answer-${index}`}
+              >
+                {incorreta}
+              </button>
             ))}
-          </>
-        )}
+          </ul>
+        </section>
       </section>
     );
   }
@@ -65,7 +93,6 @@ const mapDispatchToProps = (dispatch) => ({
 const mapStateToProps = (state) => ({
   token: state.token,
   perguntas: state.perguntas.response.results,
-  code: state.perguntas.response.response_code,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Jogo);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,9 +1,9 @@
-export const REQUEST_TOKEN = 'REQUEST_TOKEN';
-export const GET_TOKEN = 'GET_TOKEN';
-export const REQUEST_PERGUNTAS = 'REQUEST_PERGUNTAS';
-export const GET_PERGUNTAS = 'GET_PERGUNTAS';
-export const FAILED_REQUEST = 'FAILED_REQUEST';
-export const GET_EMAIL_NOME = 'GET_EMAIL_NOME';
+export const REQUEST_TOKEN = "REQUEST_TOKEN";
+export const GET_TOKEN = "GET_TOKEN";
+export const REQUEST_PERGUNTAS = "REQUEST_PERGUNTAS";
+export const GET_PERGUNTAS = "GET_PERGUNTAS";
+export const FAILED_REQUEST = "FAILED_REQUEST";
+export const GET_EMAIL_NOME = "GET_EMAIL_NOME";
 
 export function requestPerguntas() {
   return { type: REQUEST_PERGUNTAS };
@@ -25,51 +25,38 @@ function getToken(json) {
   return { type: GET_TOKEN, payload: json };
 }
 
-function failedRequest() {
-  return { type: FAILED_REQUEST };
-}
-
 const NUMBER = 3;
-
-// export function fetchToken() {
-//   return (dispatch) => {
-//     dispatch(requestToken());
-//     return fetch('https://opentdb.com/api_token.php?command=request')
-//       .then((response) => response.json())
-//       .then((json) => {
-//         if (json.response_code === NUMBER) {
-//           fetch('https://opentdb.com/api_token.php?command=request')
-//           .then((response) => response.json())
-//           .dispatch(getToken(json)
-//         }
-//         dispatch(getToken(json))}
-//       )
-//       .catch(() => dispatch(failedRequest()));
-//   };
-// }
 
 export function fetchToken() {
   return async (dispatch) => {
     dispatch(requestToken());
-    let response = await fetch('https://opentdb.com/api_token.php?command=request');
-    let res = await response.json();
-    console.log(res.response_code);
-    if (res.response_code === NUMBER) {
-      response = await fetch('https://opentdb.com/api_token.php?command=request');
-      res = await response.json();
-      dispatch(getToken(res));
-    } else {
-      dispatch(getToken(res));
-    }
+    const response = await fetch(
+      "https://opentdb.com/api_token.php?command=request"
+    );
+    const json = await response.json();
+    dispatch(getToken(json));
   };
 }
 
 export function fetchPerguntas(token) {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(requestPerguntas());
-    return fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
-      .then((response) => response.json())
-      .then((json) => dispatch(getPerguntas(json)))
-      .catch(() => dispatch(failedRequest()));
+    const response = await fetch(
+      `https://opentdb.com/api.php?amount=5&token=${token}`
+    );
+    const json = await response.json();
+    if (json.response_code === NUMBER) {
+      const response1 = await fetch(
+        "https://opentdb.com/api_token.php?command=request"
+      );
+      const json1 = await response1.json();
+      const res1 = await fetch(
+        `https://opentdb.com/api.php?amount=5&token=${json1.token}`
+      );
+      const final = await res1.json();
+      dispatch(getPerguntas(final));
+    } else {
+      dispatch(getPerguntas(json));
+    }
   };
 }
