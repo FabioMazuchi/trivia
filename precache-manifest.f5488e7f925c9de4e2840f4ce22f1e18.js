08c4097aa1c67d40a0b4422d2284ca852a12337d
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-08-31 17:45:14 -0300

    Updates

---
+++ precache-manifest.f5488e7f925c9de4e2840f4ce22f1e18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5836776e5c10eaa82b692fb67c595b7",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "4a5f21699fd415825b6d",
+    "url": "/trivia/static/css/main.deae1300.chunk.css"
+  },
+  {
+    "revision": "205d00903f86e4bc97ff",
+    "url": "/trivia/static/js/2.afe63d3c.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trivia/static/js/2.afe63d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a5f21699fd415825b6d",
+    "url": "/trivia/static/js/main.102d96c4.chunk.js"
+  },
+  {
+    "revision": "8cf3ddda99dc054ff6df",
+    "url": "/trivia/static/js/runtime-main.55dca74b.js"
+  },
+  {
+    "revision": "ccadf54377a8d01549dec31bcf771dde",
+    "url": "/trivia/static/media/github.ccadf543.png"
+  },
+  {
+    "revision": "5bb3ed2baaad92f1a4293abcd4b5e1eb",
+    "url": "/trivia/static/media/linkedin.5bb3ed2b.png"
+  }
+]);
